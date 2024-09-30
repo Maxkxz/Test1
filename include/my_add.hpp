afceac7b58039709568fe19ad8547f9b1b270f95
HW <example@gmail.com>
2024-09-30 14:15:52 +0300

    Doxyfile added

--- include/my_add.hpp
+++ include/my_add.hpp
@@ -2,6 +2,14 @@
 #ifndef MY_ADD_HPP
 #define MY_ADD_HPP
 
+/**
+* @brief The function which adds two numbers
+* @param [in] a - the first number
+* @param [in] b - the second number
+* @return the sum of a and b
+* @warning int only
+*/
+
 int my_add(int, int);
 
 #endif
